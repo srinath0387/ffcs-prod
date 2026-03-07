eb6f27a307ccec23919da72fc72e583118f7abce
Srinath Reddy <srinathreddy0387@gmail.com>
2026-03-07 20:33:50 +0530

    Restore SSL for public TCP connections

--- database.js
+++ database.js
@@ -13,6 +13,7 @@ async function initPostgres() {
   const { Pool } = require('pg');
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: process.env.DATABASE_URL.includes('.internal') ? false : { rejectUnauthorized: false },
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 5000,
